98e8192170ab73f98c5902b63d2428790b0e895b
Dhayalan <dhayalan.nadar@powerweave.com>
2019-08-23 12:26:19 +0530

    Sortlist to StatE

--- src/components/ImgList.Component.js
+++ src/components/ImgList.Component.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/bootstrap.min.css';
 
 const ImgListComponent = (props) => {
     return (
@@ -14,84 +15,56 @@ const ImgListComponent = (props) => {
                             
                             {/* FIRST COLUMN */}
                             <div className="col-md-6 text-center">
-                                    <div className="panel panel-default">
+                                    <div className="panel panel-info">
                                 <div className="panel-heading">
                                     <h3>Without Image Engine</h3>
-                                    <select className="btn btn-default">
-                                    <option value="select_image_range" selected> Select Image Range</option>    
-                                    <option value="701-706">701-710</option>
-                                    <option value="711-716">711-720</option>
-                                    <option value="721-726">721-730</option>
-                                    <option value="731-736">731-740</option>
-                                    <option value="741-746">741-750</option>
-                                    <option value="751-756">751-760</option>
-                                    <option value="761-766">761-770</option>
-                                    <option value="771-776">771-780</option>
-                                    <option value="781-786">781-790</option>
-                                    <option value="791-796">791-800</option>
+                                    <select className="btn btn-default" id="sort_list" onChange={props.sort_list}>
+                                    <option value="none" defaultValue>Filter List</option>    
+                                    <option value="701-706">701-706</option>
+                                    <option value="711-716">711-716</option>
+                                    <option value="721-726">721-726</option>
+                                    <option value="731-736">731-736</option>
+                                    <option value="741-746">741-746</option>
+                                    <option value="751-756">751-756</option>
+                                    <option value="761-766">761-766</option>
+                                    <option value="771-776">771-766</option>
+                                    <option value="781-786">781-786</option>
+                                    <option value="791-796">791-796</option>
                                     </select>
                                 </div>
                                 
                                  <div className="panel-body" id="normail-call">
-                                    <ul>
-                                     <li><img src="https://picsum.photos/id/781/400/400"  alt="" title="" /></li>
-                                     <li><img src="https://picsum.photos/id/782/400/400" alt="" title="" /></li>
-                                     <li><img src="https://picsum.photos/id/783/400/400"  alt="" title="" /></li>
-                                     <li><img src="https://picsum.photos/id/784/400/400" alt="" title="" /></li>
-                                     <li><img src="https://picsum.photos/id/785/400/400"  alt="" title="" /></li>
-                                     <li><img src="https://picsum.photos/id/786/400/400" alt="" title="" /></li>
-                                     </ul>    
-                                 </div>   
+                                    <ul>{CreateImg(props.imgData)}</ul>
+                                 </div>
                                 </div>
                             </div>
 
                             {/* SECOND COLUMN */}
                             <div className="col-md-6 text-center">
-                                <div className=" panel panel-primary">
+                                <div className="panel panel-primary">
                                 <div className="panel-heading">
                                     <h3>With Image Engine</h3>
                                    
-                                    <div id="usr-opt">
+                                   <div id="usr-opt">
                                    
-                                    <select className="btn btn-default mr-3" id="mode_dir">
-                                    <option value="directives" selected> Directives</option>    
-                                    <option value=""></option>
+                                    <select className="btn btn-default" id="mode_dir" onChange={props.changeDirective}>
+                                    <option value="none" defaultValue>Directives</option>
+                                    <option value="cmpr_80">Compression to 80</option>
+                                    <option value="r_90">Rotate 90 degrees</option>
+                                    <option value="cr_100,100,300,300">Cropping the image</option>
+                                    <option value="s_50">Sharpen Image</option>
                                    </select>
                                    
                                    <select className="btn btn-default" id="usr_fit">
-                                    <option value="fit_method" selected> Fit Method</option>    
-                                    <option value=""></option>
+                                    <option value="fit_method" defaultValue>Fit Method</option>
                                    </select>
                                    
                                    </div>
                                    </div>
                                 
                                 <div className="panel-body"  id="service-engine-call">
-                                   <ul>
-                                    <li>
-                                    <img src="http://pwrdtest.powerweaveonline.com/r_0/https://picsum.photos/id/781/400" alt="" title="" rel="preconnect" />
-                                    </li>
-                                    <li>
-                                    <img src="http://pwrdtest.powerweaveonline.com/r_0/https://picsum.photos/id/782/400" alt="" title="" rel="preconnect" />
-                                    </li>
-                                    <li>
-                                    <img src="http://pwrdtest.powerweaveonline.com/r_0/https://picsum.photos/id/783/400" alt="" title="" rel="preconnect" />
-                                    </li>
-                                    <li>
-                                    <img src="http://pwrdtest.powerweaveonline.com/r_0/https://picsum.photos/id/784/400" alt="" title="" rel="preconnect" />
-                                    </li>
-                                    <li>
-                                    <img src="http://pwrdtest.powerweaveonline.com/r_0/https://picsum.photos/id/785/400" alt="" title="" rel="preconnect" />
-                                    </li>
-                                    <li>
-                                    <img src="http://pwrdtest.powerweaveonline.com/r_0/https://picsum.photos/id/786/400" alt="" title="" rel="preconnect" />
-                                    </li>
-                                    </ul>     
-                                </div>   
-
-                                {/* <input type="file" id="file_upload" onChange={props.fileUpload} /> */}
-                                {/* <button id="btnUpload" onClick={props.clickHandle}>Upload</button> */}
-                                {/* <img id="img2" src={require('../css/images/15.png')} className="img-responsive" alt="" /> */}
+                                    <ul id="ulImgEng">{CreateImg(props.imgEngData)}</ul>
+                                </div>
 
                                 </div>
                             </div>
@@ -105,3 +78,10 @@ const ImgListComponent = (props) => {
     )
 }
 export default ImgListComponent;
+
+
+export const CreateImg = (data) => {
+    return (
+        data.map(info => <li key={info.id}><img id={info.id} src={info.imgUrl}  alt="" title="" /></li>)        
+    )
+};
--- src/components/ImgList.Container.js
+++ src/components/ImgList.Container.js
@@ -4,27 +4,78 @@ import ImgListComponent from './ImgList.Component';
 export default class ImgListContainer extends Component {
     constructor(props){
         super(props);
-        this.clickHandle = this.clickHandle.bind(this);
-    }
-
-    componentWillMount(){
-        //console.log('componentWillMount');
+        this.sort_list = this.sort_list.bind(this);
+        this.changeDirective = this.changeDirective.bind(this);
+        this.state = {
+            imgRange: {start:375,end:380},
+            imgData: [],
+            imgEngData: []
+        };
     }
 
     componentDidMount(){
-        //console.log('componentDidMount');
+        // console.log('componentWillMount');
+        let objImg=[], objImgEng=[], imgRangeStart=0, imgRangeEnd=0, loopLength=100;
+        imgRangeStart = this.state.imgRange['start'];
+        imgRangeEnd = this.state.imgRange.end;
+        loopLength = imgRangeEnd-imgRangeStart;
+        for (var i = 0; i <= loopLength; i++) {
+            objImg[i] = {imgUrl:`https://picsum.photos/id/${imgRangeStart}/250`,id: imgRangeStart};
+            objImgEng[i] = {imgUrl:`http://pwrdtest.powerweaveonline.com/r_0/https://picsum.photos/id/${imgRangeStart}/250`,id: imgRangeStart};
+            imgRangeStart++;
+        }
+        this.setState((state, props) => ({
+            ...state,
+            ...state.imgData = objImg,
+            ...state.imgEngData = objImgEng
+        }));
     }
 
-    clickHandle(e){
+    changeDirective(e){
         e.preventDefault();
-        console.log('clickHandle',e);
+        console.log('changeDirective',e.target.value);
+        debugger;
+        //var x = document.getElementById("mySelect").value;
+        let imgUrl="http://pwrdtest.powerweaveonline.com/r_0/https://picsum.photos/id/100/400";
+        var ul = document.getElementById("ulImgEng");
+        var items = ul.getElementsByTagName("li");
+        for (var i = 0; i < items.length; ++i) {
+            let img = items[i].getElementsByTagName('img')[0];
+            //items[i].firstElementChild.src = items[i].firstElementChild.src.replace('r_0',e.target.value);
+            img.src = img.src.replace('r_0',e.target.value);
+        }
+        if (e.target.selectedIndex===10){
+            var x = e.target.value;
+            document.getElementById("demo").innerHTML = "You selected: " + x;
+            document.getElementById('img1')
+            .setAttribute(
+                'src', `${imgUrl.replace('r_0',x)}`
+            );
+        }
+    }
+    
+    sort_list(e){
+        e.preventDefault();
+        // console.log('count_list',e.target.value.split(" "));
+        let range = e.target.value.split("-");
+        // console.log('Sort Image list', range);
+        var first = range[0],second = range[1];
+        // console.log(first, second);
+        this.setState((state) => ({
+            ...state,
+            ...state.imgRange = {start:first,end:second}
+        }));
     }
 
-    render() { 
-        return (            
+    render() {
+        return (
             <React.Fragment>
                 <ImgListComponent
-                    clickHandle = {this.clickHandle}
+                    imgRange={this.state.imgRange}
+                    imgData = {this.state.imgData}
+                    imgEngData = {this.state.imgEngData}
+                    sort_list= {this.sort_list}
+                    changeDirective = {this.changeDirective}
                 />
             </React.Fragment>
          );
