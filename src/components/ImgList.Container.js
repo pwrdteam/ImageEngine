6a782da118fe0e3400f3b7b9bfd17272ff52ccaf
Dhayalan <dhayalan.nadar@powerweave.com>
2019-08-23 11:15:19 +0530

    UI Changes

--- src/components/ImgList.Container.js
+++ src/components/ImgList.Container.js
@@ -4,11 +4,7 @@ import ImgListComponent from './ImgList.Component';
 export default class ImgListContainer extends Component {
     constructor(props){
         super(props);
-        this.fileUpload = this.fileUpload.bind(this);
         this.clickHandle = this.clickHandle.bind(this);
-        this.state = {
-            totalImgs: 0,
-        };
     }
 
     componentWillMount(){
@@ -19,29 +15,6 @@ export default class ImgListContainer extends Component {
         //console.log('componentDidMount');
     }
 
-    fileUpload(el){
-        el.preventDefault();
-        //console.log('fileUpload',el);
-        debugger;
-        var F = el.target.files[0];
-        var reader = new FileReader();
-        reader.onload = (e) => {
-            var img = new Image();
-            img.onload = function(el){
-                let canImg1 = document.getElementById('canImg1');
-                let contextImg1 = canImg1.getContext('2d');
-                canImg1.width  = this.width;
-                canImg1.height = this.height;
-                contextImg1.drawImage(this, 0, 0);
-            };
-            img.src = e.target.result;
-            document.getElementById('img1')
-            .setAttribute(
-                'src', img.src
-            );
-        };
-        reader.readAsDataURL(F);
-    }
     clickHandle(e){
         e.preventDefault();
         console.log('clickHandle',e);
@@ -51,7 +24,6 @@ export default class ImgListContainer extends Component {
         return (            
             <React.Fragment>
                 <ImgListComponent
-                    fileUpload = {this.fileUpload}
                     clickHandle = {this.clickHandle}
                 />
             </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/custom.css';
+//import App from './App';
 import ImgListContainer from './components/ImgList.Container';
 import * as serviceWorker from './serviceWorker';
 
